dcbfb8ee67323014098df9fb5a535d1099e2e2ec
Heiko Carstens <heiko.carstens@de.ibm.com>
2006-05-11 07:03:29 +0200

    update s390 syscalls, add script

--- sysdeps/linux-gnu/s390/syscalls31.h
+++ sysdeps/linux-gnu/s390/syscalls31.h
@@ -1,283 +1,304 @@
-"0", "exit",
-    "fork",
-    "read",
-    "write",
-    "open",
-    "close",
-    "7",
-    "creat",
-    "link",
-    "unlink",
-    "execve",
-    "chdir",
-    "time",
-    "mknod",
-    "chmod",
-    "lchown16",
-    "17",
-    "18",
-    "lseek",
-    "getpid",
-    "mount",
-    "oldumount",
-    "setuid16",
-    "getuid16",
-    "stime",
-    "ptrace",
-    "alarm",
-    "28",
-    "pause",
-    "utime",
-    "31",
-    "32",
-    "access",
-    "nice",
-    "35",
-    "sync",
-    "kill",
-    "rename",
-    "mkdir",
-    "rmdir",
-    "dup",
-    "pipe",
-    "times",
-    "44",
-    "brk",
-    "setgid16",
-    "getgid16",
-    "signal",
-    "geteuid16",
-    "getegid16",
-    "acct",
-    "umount",
-    "53",
-    "ioctl",
-    "fcntl",
-    "56",
-    "setpgid",
-    "58",
-    "59",
-    "umask",
-    "chroot",
-    "ustat",
-    "dup2",
-    "getppid",
-    "getpgrp",
-    "setsid",
-    "sigaction",
-    "68",
-    "69",
-    "setreuid16",
-    "setregid16",
-    "sigsuspend",
-    "sigpending",
-    "sethostname",
-    "setrlimit",
-    "old_getrlimit",
-    "getrusage",
-    "gettimeofday",
-    "settimeofday",
-    "getgroups16",
-    "setgroups16",
-    "82",
-    "symlink",
-    "84",
-    "readlink",
-    "uselib",
-    "swapon",
-    "reboot",
-    "89",
-    "old_mmap",
-    "munmap",
-    "truncate",
-    "ftruncate",
-    "fchmod",
-    "fchown16",
-    "getpriority",
-    "setpriority",
-    "98",
-    "statfs",
-    "fstatfs",
-    "101",
-    "socketcall",
-    "syslog",
-    "setitimer",
-    "getitimer",
-    "newstat",
-    "newlstat",
-    "newfstat",
-    "109",
-    "lookup_dcookie",
-    "vhangup",
-    "112",
-    "113",
-    "wait4",
-    "swapoff",
-    "sysinfo",
-    "ipc",
-    "fsync",
-    "sigreturn",
-    "clone",
-    "setdomainname",
-    "newuname",
-    "123",
-    "adjtimex",
-    "mprotect",
-    "sigprocmask",
-    "127",
-    "init_module",
-    "delete_module",
-    "130",
-    "quotactl",
-    "getpgid",
-    "fchdir",
-    "bdflush",
-    "sysfs",
-    "personality",
-    "137",
-    "setfsuid16",
-    "setfsgid16",
-    "llseek",
-    "getdents",
-    "select",
-    "flock",
-    "msync",
-    "readv",
-    "writev",
-    "getsid",
-    "fdatasync",
-    "sysctl",
-    "mlock",
-    "munlock",
-    "mlockall",
-    "munlockall",
-    "sched_setparam",
-    "sched_getparam",
-    "sched_setscheduler",
-    "sched_getscheduler",
-    "sched_yield",
-    "sched_get_priority_max",
-    "sched_get_priority_min",
-    "sched_rr_get_interval",
-    "nanosleep",
-    "mremap",
-    "setresuid16",
-    "getresuid16",
-    "166",
-    "167",
-    "poll",
-    "nfsservctl",
-    "setresgid16",
-    "getresgid16",
-    "prctl",
-    "rt_sigreturn",
-    "rt_sigaction",
-    "rt_sigprocmask",
-    "rt_sigpending",
-    "rt_sigtimedwait",
-    "rt_sigqueueinfo",
-    "rt_sigsuspend",
-    "pread64",
-    "pwrite64",
-    "chown16",
-    "getcwd",
-    "capget",
-    "capset",
-    "sigaltstack",
-    "sendfile",
-    "188",
-    "189",
-    "vfork",
-    "getrlimit",
-    "mmap2",
-    "truncate64",
-    "ftruncate64",
-    "stat64",
-    "lstat64",
-    "fstat64",
-    "lchown",
-    "getuid",
-    "getgid",
-    "geteuid",
-    "getegid",
-    "setreuid",
-    "setregid",
-    "getgroups",
-    "setgroups",
-    "fchown",
-    "setresuid",
-    "getresuid",
-    "setresgid",
-    "getresgid",
-    "chown",
-    "setuid",
-    "setgid",
-    "setfsuid",
-    "setfsgid",
-    "pivot_root",
-    "mincore",
-    "madvise",
-    "getdents64",
-    "fcntl64",
-    "readahead",
-    "sendfile64",
-    "setxattr",
-    "lsetxattr",
-    "fsetxattr",
-    "getxattr",
-    "lgetxattr",
-    "fgetxattr",
-    "listxattr",
-    "llistxattr",
-    "flistxattr",
-    "removexattr",
-    "lremovexattr",
-    "fremovexattr",
-    "gettid",
-    "tkill",
-    "futex",
-    "sched_setaffinity",
-    "sched_getaffinity",
-    "tgkill",
-    "242",
-    "io_setup",
-    "io_destroy",
-    "io_getevents",
-    "io_submit",
-    "io_cancel",
-    "exit_group",
-    "epoll_create",
-    "epoll_ctl",
-    "epoll_wait",
-    "set_tid_address",
-    "fadvise64",
-    "timer_create",
-    "timer_settime",
-    "timer_gettime",
-    "timer_getoverrun",
-    "timer_delete",
-    "clock_settime",
-    "clock_gettime",
-    "clock_getres",
-    "clock_nanosleep",
-    "263",
-    "fadvise64_64",
-    "statfs64",
-    "fstatfs64",
-    "remap_file_pages",
-    "268",
-    "269",
-    "270",
-    "mq_open",
-    "mq_unlink",
-    "mq_timedsend",
-    "mq_timedreceive",
-    "mq_notify",
-    "mq_getsetattr",
-    "kexec_load",
-    "add_key",
-    "request_key",
-    "keyctl",
-    "waitid",
-    "ioprio_set",
-    "ioprio_get", "inotify_init", "inotify_add_watch", "inotify_rm_watch",
+    "0",       			 /* 0 */
+    "exit",    			 /* 1 */
+    "fork",    			 /* 2 */
+    "read",    			 /* 3 */
+    "write",   			 /* 4 */
+    "open",    			 /* 5 */
+    "close",   			 /* 6 */
+    "restart_syscall", 		 /* 7 */
+    "creat",   			 /* 8 */
+    "link",    			 /* 9 */
+    "unlink",  			 /* 10 */
+    "execve",  			 /* 11 */
+    "chdir",   			 /* 12 */
+    "time",    			 /* 13 */
+    "mknod",   			 /* 14 */
+    "chmod",   			 /* 15 */
+    "lchown16",			 /* 16 */
+    "17",      			 /* 17 */
+    "18",      			 /* 18 */
+    "lseek",   			 /* 19 */
+    "getpid",  			 /* 20 */
+    "mount",   			 /* 21 */
+    "oldumount",       		 /* 22 */
+    "setuid16",			 /* 23 */
+    "getuid16",			 /* 24 */
+    "stime",   			 /* 25 */
+    "ptrace",  			 /* 26 */
+    "alarm",   			 /* 27 */
+    "28",      			 /* 28 */
+    "pause",   			 /* 29 */
+    "utime",   			 /* 30 */
+    "31",      			 /* 31 */
+    "32",      			 /* 32 */
+    "access",  			 /* 33 */
+    "nice",    			 /* 34 */
+    "35",      			 /* 35 */
+    "sync",    			 /* 36 */
+    "kill",    			 /* 37 */
+    "rename",  			 /* 38 */
+    "mkdir",   			 /* 39 */
+    "rmdir",   			 /* 40 */
+    "dup",     			 /* 41 */
+    "pipe",    			 /* 42 */
+    "times",   			 /* 43 */
+    "44",      			 /* 44 */
+    "brk",     			 /* 45 */
+    "setgid16",			 /* 46 */
+    "getgid16",			 /* 47 */
+    "signal",  			 /* 48 */
+    "geteuid16",       		 /* 49 */
+    "getegid16",       		 /* 50 */
+    "acct",    			 /* 51 */
+    "umount",  			 /* 52 */
+    "53",      			 /* 53 */
+    "ioctl",   			 /* 54 */
+    "fcntl",   			 /* 55 */
+    "56",      			 /* 56 */
+    "setpgid", 			 /* 57 */
+    "58",      			 /* 58 */
+    "59",      			 /* 59 */
+    "umask",   			 /* 60 */
+    "chroot",  			 /* 61 */
+    "ustat",   			 /* 62 */
+    "dup2",    			 /* 63 */
+    "getppid", 			 /* 64 */
+    "getpgrp", 			 /* 65 */
+    "setsid",  			 /* 66 */
+    "sigaction",       		 /* 67 */
+    "68",      			 /* 68 */
+    "69",      			 /* 69 */
+    "setreuid16",      		 /* 70 */
+    "setregid16",      		 /* 71 */
+    "sigsuspend",      		 /* 72 */
+    "sigpending",      		 /* 73 */
+    "sethostname",     		 /* 74 */
+    "setrlimit",       		 /* 75 */
+    "old_getrlimit",   		 /* 76 */
+    "getrusage",       		 /* 77 */
+    "gettimeofday",    		 /* 78 */
+    "settimeofday",    		 /* 79 */
+    "getgroups16",     		 /* 80 */
+    "setgroups16",     		 /* 81 */
+    "82",      			 /* 82 */
+    "symlink", 			 /* 83 */
+    "84",      			 /* 84 */
+    "readlink",			 /* 85 */
+    "uselib",  			 /* 86 */
+    "swapon",  			 /* 87 */
+    "reboot",  			 /* 88 */
+    "89",      			 /* 89 */
+    "old_mmap",			 /* 90 */
+    "munmap",  			 /* 91 */
+    "truncate",			 /* 92 */
+    "ftruncate",       		 /* 93 */
+    "fchmod",  			 /* 94 */
+    "fchown16",			 /* 95 */
+    "getpriority",     		 /* 96 */
+    "setpriority",     		 /* 97 */
+    "98",      			 /* 98 */
+    "statfs",  			 /* 99 */
+    "fstatfs", 			 /* 100 */
+    "101",     			 /* 101 */
+    "socketcall",      		 /* 102 */
+    "syslog",  			 /* 103 */
+    "setitimer",       		 /* 104 */
+    "getitimer",       		 /* 105 */
+    "newstat", 			 /* 106 */
+    "newlstat",			 /* 107 */
+    "newfstat",			 /* 108 */
+    "109",     			 /* 109 */
+    "lookup_dcookie",  		 /* 110 */
+    "vhangup", 			 /* 111 */
+    "112",     			 /* 112 */
+    "113",     			 /* 113 */
+    "wait4",   			 /* 114 */
+    "swapoff", 			 /* 115 */
+    "sysinfo", 			 /* 116 */
+    "ipc",     			 /* 117 */
+    "fsync",   			 /* 118 */
+    "sigreturn",       		 /* 119 */
+    "clone",   			 /* 120 */
+    "setdomainname",   		 /* 121 */
+    "newuname",			 /* 122 */
+    "123",     			 /* 123 */
+    "adjtimex",			 /* 124 */
+    "mprotect",			 /* 125 */
+    "sigprocmask",     		 /* 126 */
+    "127",     			 /* 127 */
+    "init_module",     		 /* 128 */
+    "delete_module",   		 /* 129 */
+    "130",     			 /* 130 */
+    "quotactl",			 /* 131 */
+    "getpgid", 			 /* 132 */
+    "fchdir",  			 /* 133 */
+    "bdflush", 			 /* 134 */
+    "sysfs",   			 /* 135 */
+    "personality",     		 /* 136 */
+    "137",     			 /* 137 */
+    "setfsuid16",      		 /* 138 */
+    "setfsgid16",      		 /* 139 */
+    "llseek",  			 /* 140 */
+    "getdents",			 /* 141 */
+    "select",  			 /* 142 */
+    "flock",   			 /* 143 */
+    "msync",   			 /* 144 */
+    "readv",   			 /* 145 */
+    "writev",  			 /* 146 */
+    "getsid",  			 /* 147 */
+    "fdatasync",       		 /* 148 */
+    "sysctl",  			 /* 149 */
+    "mlock",   			 /* 150 */
+    "munlock", 			 /* 151 */
+    "mlockall",			 /* 152 */
+    "munlockall",      		 /* 153 */
+    "sched_setparam",  		 /* 154 */
+    "sched_getparam",  		 /* 155 */
+    "sched_setscheduler",      	 /* 156 */
+    "sched_getscheduler",      	 /* 157 */
+    "sched_yield",     		 /* 158 */
+    "sched_get_priority_max",  	 /* 159 */
+    "sched_get_priority_min",  	 /* 160 */
+    "sched_rr_get_interval",   	 /* 161 */
+    "nanosleep",       		 /* 162 */
+    "mremap",  			 /* 163 */
+    "setresuid16",     		 /* 164 */
+    "getresuid16",     		 /* 165 */
+    "166",     			 /* 166 */
+    "167",     			 /* 167 */
+    "poll",    			 /* 168 */
+    "nfsservctl",      		 /* 169 */
+    "setresgid16",     		 /* 170 */
+    "getresgid16",     		 /* 171 */
+    "prctl",   			 /* 172 */
+    "rt_sigreturn",    		 /* 173 */
+    "rt_sigaction",    		 /* 174 */
+    "rt_sigprocmask",  		 /* 175 */
+    "rt_sigpending",   		 /* 176 */
+    "rt_sigtimedwait", 		 /* 177 */
+    "rt_sigqueueinfo", 		 /* 178 */
+    "rt_sigsuspend",   		 /* 179 */
+    "pread64", 			 /* 180 */
+    "pwrite64",			 /* 181 */
+    "chown16", 			 /* 182 */
+    "getcwd",  			 /* 183 */
+    "capget",  			 /* 184 */
+    "capset",  			 /* 185 */
+    "sigaltstack",     		 /* 186 */
+    "sendfile",			 /* 187 */
+    "188",     			 /* 188 */
+    "189",     			 /* 189 */
+    "vfork",   			 /* 190 */
+    "getrlimit",       		 /* 191 */
+    "mmap2",   			 /* 192 */
+    "truncate64",      		 /* 193 */
+    "ftruncate64",     		 /* 194 */
+    "stat64",  			 /* 195 */
+    "lstat64", 			 /* 196 */
+    "fstat64", 			 /* 197 */
+    "lchown",  			 /* 198 */
+    "getuid",  			 /* 199 */
+    "getgid",  			 /* 200 */
+    "geteuid", 			 /* 201 */
+    "getegid", 			 /* 202 */
+    "setreuid",			 /* 203 */
+    "setregid",			 /* 204 */
+    "getgroups",       		 /* 205 */
+    "setgroups",       		 /* 206 */
+    "fchown",  			 /* 207 */
+    "setresuid",       		 /* 208 */
+    "getresuid",       		 /* 209 */
+    "setresgid",       		 /* 210 */
+    "getresgid",       		 /* 211 */
+    "chown",   			 /* 212 */
+    "setuid",  			 /* 213 */
+    "setgid",  			 /* 214 */
+    "setfsuid",			 /* 215 */
+    "setfsgid",			 /* 216 */
+    "pivot_root",      		 /* 217 */
+    "mincore", 			 /* 218 */
+    "madvise", 			 /* 219 */
+    "getdents64",      		 /* 220 */
+    "fcntl64", 			 /* 221 */
+    "readahead",       		 /* 222 */
+    "sendfile64",      		 /* 223 */
+    "setxattr",			 /* 224 */
+    "lsetxattr",       		 /* 225 */
+    "fsetxattr",       		 /* 226 */
+    "getxattr",			 /* 227 */
+    "lgetxattr",       		 /* 228 */
+    "fgetxattr",       		 /* 229 */
+    "listxattr",       		 /* 230 */
+    "llistxattr",      		 /* 231 */
+    "flistxattr",      		 /* 232 */
+    "removexattr",     		 /* 233 */
+    "lremovexattr",    		 /* 234 */
+    "fremovexattr",    		 /* 235 */
+    "gettid",  			 /* 236 */
+    "tkill",   			 /* 237 */
+    "futex",   			 /* 238 */
+    "sched_setaffinity",       	 /* 239 */
+    "sched_getaffinity",       	 /* 240 */
+    "tgkill",  			 /* 241 */
+    "242",     			 /* 242 */
+    "io_setup",			 /* 243 */
+    "io_destroy",      		 /* 244 */
+    "io_getevents",    		 /* 245 */
+    "io_submit",       		 /* 246 */
+    "io_cancel",       		 /* 247 */
+    "exit_group",      		 /* 248 */
+    "epoll_create",    		 /* 249 */
+    "epoll_ctl",       		 /* 250 */
+    "epoll_wait",      		 /* 251 */
+    "set_tid_address", 		 /* 252 */
+    "fadvise64",       		 /* 253 */
+    "timer_create",    		 /* 254 */
+    "timer_settime",   		 /* 255 */
+    "timer_gettime",   		 /* 256 */
+    "timer_getoverrun",		 /* 257 */
+    "timer_delete",    		 /* 258 */
+    "clock_settime",   		 /* 259 */
+    "clock_gettime",   		 /* 260 */
+    "clock_getres",    		 /* 261 */
+    "clock_nanosleep", 		 /* 262 */
+    "263",     			 /* 263 */
+    "fadvise64_64",    		 /* 264 */
+    "statfs64",			 /* 265 */
+    "fstatfs64",       		 /* 266 */
+    "remap_file_pages",		 /* 267 */
+    "268",     			 /* 268 */
+    "269",     			 /* 269 */
+    "270",     			 /* 270 */
+    "mq_open", 			 /* 271 */
+    "mq_unlink",       		 /* 272 */
+    "mq_timedsend",    		 /* 273 */
+    "mq_timedreceive", 		 /* 274 */
+    "mq_notify",       		 /* 275 */
+    "mq_getsetattr",   		 /* 276 */
+    "kexec_load",      		 /* 277 */
+    "add_key", 			 /* 278 */
+    "request_key",     		 /* 279 */
+    "keyctl",  			 /* 280 */
+    "waitid",  			 /* 281 */
+    "ioprio_set",      		 /* 282 */
+    "ioprio_get",      		 /* 283 */
+    "inotify_init",    		 /* 284 */
+    "inotify_add_watch",       	 /* 285 */
+    "inotify_rm_watch",		 /* 286 */
+    "287",     			 /* 287 */
+    "openat",  			 /* 288 */
+    "mkdirat", 			 /* 289 */
+    "mknodat", 			 /* 290 */
+    "fchownat",			 /* 291 */
+    "futimesat",       		 /* 292 */
+    "fstatat64",       		 /* 293 */
+    "unlinkat",			 /* 294 */
+    "renameat",			 /* 295 */
+    "linkat",  			 /* 296 */
+    "symlinkat",       		 /* 297 */
+    "readlinkat",      		 /* 298 */
+    "fchmodat",			 /* 299 */
+    "faccessat",       		 /* 300 */
+    "pselect6",			 /* 301 */
+    "ppoll",   			 /* 302 */
+    "unshare", 			 /* 303 */
