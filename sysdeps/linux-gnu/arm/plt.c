d568f51920d5b2e0813cb5bb7e3cca10fc404623
Zach Welch <zwelch@codesourcery.com>
2010-10-08 14:44:16 -0700

    Fix ARM PLT lookups.
    An ARM PLT may not contain fixed-length records, because Thumb stubs may
    be added to provide interworking.  These changes ensure the breakpoint
    gets set on the ARM portion of PLT entries while skipping any Thumb
    stubs that may be present.  This patch makes the testsuite work when
    built for ARMv7a and Thumb-2.
    
    Signed-off-by: Zachary T Welch <zwelch@codesourcery.com>

--- sysdeps/linux-gnu/arm/plt.c
+++ sysdeps/linux-gnu/arm/plt.c
@@ -1,9 +1,21 @@
 #include <gelf.h>
 #include "common.h"
 
+static int
+arch_plt_entry_has_stub(struct ltelf *lte, size_t off) {
+	uint16_t op = *(uint16_t *)((char *)lte->relplt->d_buf + off);
+	return op == 0x4778;
+}
+
 GElf_Addr
 arch_plt_sym_val(struct ltelf *lte, size_t ndx, GElf_Rela * rela) {
-	return lte->plt_addr + 20 + ndx * 12;
+	size_t start = lte->relplt->d_size + 12;
+	size_t off = start + 20, i;
+	for (i = 0; i < ndx; i++)
+		off += arch_plt_entry_has_stub(lte, off) ? 16 : 12;
+	if (arch_plt_entry_has_stub(lte, off))
+		off += 4;
+	return lte->plt_addr + off - start;
 }
 
 void *
