12fe1f884fc0c32ad65fcc6904c1b2d47a31f670
Petr Machata <pmachata@redhat.com>
2010-10-18 17:30:09 +0200

    Support fork/exec syscalls on 31-bit s390

--- sysdeps/linux-gnu/s390/arch.h
+++ sysdeps/linux-gnu/s390/arch.h
@@ -12,6 +12,11 @@
 #define LT_ELF_MACHINE	EM_S390
 #define LT_ELFCLASS2	ELFCLASS32
 #define LT_ELF_MACHINE2	EM_S390
+
+/* __NR_fork, __NR_clone, __NR_clone2, __NR_vfork and __NR_execve
+   from asm-s390/unistd.h.  */
+#define FORK_EXEC_SYSCALLS , { 2, 120, -1, 190, 11 }
+
 #else
 #define LT_ELFCLASS	ELFCLASS32
 #define LT_ELF_MACHINE	EM_S390
