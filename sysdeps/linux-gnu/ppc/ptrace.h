4c05dd7c188c8625056e482d0a023b778cb400e4
Petr Machata <pmachata@redhat.com>
2010-10-25 13:39:28 +0200

    Fix save_register_args and related on ppc64

--- sysdeps/linux-gnu/ppc/ptrace.h
+++ sysdeps/linux-gnu/ppc/ptrace.h
@@ -1,1 +1,11 @@
 #include <sys/ptrace.h>
+#include <sys/ucontext.h>
+typedef struct {
+  int valid;
+  unsigned long func_args[6];
+  unsigned long sysc_args[6];
+  gregset_t regs;
+  fpregset_t fpregs;
+  gregset_t regs_copy;
+  fpregset_t fpregs_copy;
+} proc_archdep;
