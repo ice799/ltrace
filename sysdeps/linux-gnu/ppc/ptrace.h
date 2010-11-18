87e1a5cc080cdab488a6c7076aa5c86dfe1c26f0
Petr Machata <pmachata@redhat.com>
2010-11-18 11:19:09 +0100

    Fix compilation issue on ppc32

--- sysdeps/linux-gnu/ppc/ptrace.h
+++ sysdeps/linux-gnu/ppc/ptrace.h
@@ -1,9 +1,14 @@
 #include <sys/ptrace.h>
 #include <sys/ucontext.h>
+
+#ifdef __powerpc64__
+#define GET_FPREG(RS, R) ((RS)[R])
+#else
+#define GET_FPREG(RS, R) ((RS).fpregs[R])
+#endif
+
 typedef struct {
   int valid;
-  unsigned long func_args[6];
-  unsigned long sysc_args[6];
   gregset_t regs;
   fpregset_t fpregs;
   gregset_t regs_copy;
